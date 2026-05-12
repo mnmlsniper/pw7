b6c67c8b6d86a77237db7a2b8024021a03df1e29
sniper <mnml.sniper@gmail.com>
2026-05-12 21:03:37 +0300

    fix

---
+++ tests/example.spec.js
@@ -0,0 +1,26 @@
+// чего-то импортируем
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  // Arrange 
+  // WHEN Когда пользователь открывает сайт
+  // Предусловие сайт
+  // Открыть сайт
+  await page.goto('https://todomvc.com/examples/vue/dist/#/');
+
+  // Act
+  // GIVEN И вводит описание задачи
+  // Шаги теста
+  // Выполнить шаги теста
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).click();
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('Выгулять собак');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
+  
+  // Assert
+  // Then Тогда отображается введенная задача
+  // Проверка ожидаемого и фактического результата
+  // Проверить ожидаемый результат и фактический
+  await expect(page.getByRole('main')).toContainText('Выгулять собак');
+
+
+});

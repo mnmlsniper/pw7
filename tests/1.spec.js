3e9baf154d2a53c169ca618e737195858a1c8819
sniper <mnml.sniper@gmail.com>
2026-05-18 20:52:21 +0300

    add realworld test

---
+++ tests/1.spec.js
@@ -0,0 +1,37 @@
+import { test, expect } from '@playwright/test';
+
+const URL = 'https://realworld.qa.guru/';
+let username = 'sniper180526';
+let email = 'sniper180526@ya.ru';
+let password = 'Sniper12';
+
+function getUrl (username) {
+    console.log(`Пользователь ${username} собирается зарегистрироваться на сайте ${URL}`);
+    return URL;
+}
+
+  const url = getUrl(username);
+
+
+test('Пользователь может зарегистрироваться используя email и пароль', async ({ page }) => {
+  
+  await page.goto(url);
+  await page.getByRole('link', { name: 'Sign up' }).click();
+
+  await page.getByRole('textbox', { name: 'Your Name' }).click();
+  //
+  await page.getByRole('textbox', { name: 'Your Name' }).fill(username);
+
+  await page.getByRole('textbox', { name: 'Email' }).click();
+  //
+  await page.getByRole('textbox', { name: 'Email' }).fill(email);
+
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  //
+  await page.getByRole('textbox', { name: 'Password' }).fill(password);
+  await page.getByRole('button', { name: 'Sign up' }).click();
+  //
+  await expect(page.getByRole('navigation')).toContainText(username);
+});
+
+// данные для захордкожены
